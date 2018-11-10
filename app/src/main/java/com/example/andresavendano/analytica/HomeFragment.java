68df0394f4cafc0fa53b30652d25fd3d9e8c7ab1
Camila White <camilawhite@MacBook-Pro-Camila.local>
2018-11-10 15:48:33 -0500

    Subiendo home

--- app/src/main/java/com/example/andresavendano/analytica/HomeFragment.java
+++ app/src/main/java/com/example/andresavendano/analytica/HomeFragment.java
@@ -1,17 +1,34 @@
 package com.example.andresavendano.analytica;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class HomeFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,null);
+        View v = inflater.inflate(R.layout.fragment_home,null);
+
+        TextView txt= v.findViewById(R.id.link); //txt is object of TextView
+        txt.setMovementMethod(LinkMovementMethod.getInstance());
+        txt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW);
+                browserIntent.setData(Uri.parse("https://sites.google.com/site/analisisnumerico20182aamw/home"));
+                startActivity(browserIntent);
+            }
+        });
+
+        return v;
     }
 }
